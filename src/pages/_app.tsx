5c0d05e6cf3cb63cac4ede41b1cd64b3659e5f9f
Hayk Abrahamyan <abrahamyanhayk01@gmail.com>
2022-03-08 18:10:23 +0400

    Added main layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import { Provider } from 'react-redux';
-import Head from 'next/head';
+import type { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
@@ -11,25 +12,29 @@ import store from '../app/store';
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
+type NextPageWithLayout = NextPage & {
+    getLayout?: (page: ReactElement) => ReactNode
+}
+
 interface MyAppProps extends AppProps {
     emotionCache?: EmotionCache;
+    Component: NextPageWithLayout
 }
 
 
 const MyApp = (props: MyAppProps) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout || ((page) => page);
+
   return (
       <CacheProvider value={emotionCache}>
-          <Head>
-              <meta name="viewport" content="initial-scale=1, width=device-width" />
-              <title>Hot Coffee</title>
-          </Head>
           <Provider store={store}>
               <ThemeProvider theme={theme}>
                   {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
                   <CssBaseline />
-                  <Component {...pageProps} />
+                  {getLayout(<Component {...pageProps} />)}
               </ThemeProvider>
           </Provider>
       </CacheProvider>
