f50f09648d822821109a60ad9c237fb1204e238a
Hayk Abrahamyan <abrahamyanhayk01@gmail.com>
2022-03-13 23:40:51 +0400

    Create home page intro section

---
+++ src/features/Discover/useStyles.ts
@@ -0,0 +1,7 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(theme => ({
+    image: {
+        backgroundImage: `url(https://preview.colorlib.com/theme/coffeeblend/images/xabout.jpg.pagespeed.ic.1CZ91rANlw.webp)`,
+    },
+}));
--- src/features/HomeBanner/index.tsx
+++ src/features/HomeBanner/index.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image';
+import homeBannerImage from '../../static/images/bg_3.jpg';
 
 export default function HomeBanner (): JSX.Element {
     return (
         <Image
-            src="https://images.unsplash.com/photo-1484950763426-56b5bf172dbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
+            src={homeBannerImage}
             alt="Picture of the author"
             layout="fill"
-            objectFit='cover'
-            width="1000"
-            height="1000"
+            objectFit="cover"
+            priority
         />
     )
 }
