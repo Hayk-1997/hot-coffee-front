c949312e5bc9007a1485c5e3b7636b87fe7b0adc
Hayk Abrahamyan <abrahamyanhayk01@gmail.com>
2022-03-14 23:39:26 +0400

    Chage swc configuration

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,14 +3,12 @@ import MainLayout from '../../layouts/mainLayout';
 import HomeBanner from '../../features/HomeBanner';
 import Intro from '../../features/Intro';
 import Discovery from '../../features/Discover';
-import useStyles from './useStyles';
 
 export default function Home (): JSX.Element {
-    const styles = useStyles();
 
     return (
        <>
-           <section className={styles.homeBanner}>
+           <section>
                <HomeBanner />
            </section>
            <section className="ftco-intro">
--- src/pages/home/useStyles.ts
+++
@@ -1,7 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles';
-
-export default makeStyles(theme => ({
-    homeBanner: {
-        height: '100vh',
-    },
-}));
