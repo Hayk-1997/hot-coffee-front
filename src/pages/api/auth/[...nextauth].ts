f24514106a4220e6647aae9b47e96fb9a74a7f8e
Hayk Abrahamyan <abrahamyanhayk01@gmail.com>
2022-04-02 18:38:48 +0400

    Added Next-Auth2 configurations

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,22 @@
+import NextAuth from 'next-auth';
+import FacebookProvider from 'next-auth/providers/facebook';
+// import Auth0Provider from 'next-auth/providers/auth0';
+import GoogleProvider from 'next-auth/providers/google';
+
+export default NextAuth({
+    providers: [
+        FacebookProvider({
+            clientId: process.env.FACEBOOK_ID,
+            clientSecret: process.env.FACEBOOK_SECRET
+        }),
+        // Auth0Provider({
+        //     clientId: process.env.AUTH0_CLIENT_ID,
+        //     clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        //     issuer: process.env.AUTH0_ISSUER_BASE_URL,
+        // }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+    ]
+})
