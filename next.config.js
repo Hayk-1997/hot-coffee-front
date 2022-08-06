01d8b4bab8e175424be68774fa239bbc73e1bd1f
Hayk Abrahamyan <abrahamyanhayk01@gmail.com>
2022-08-07 01:45:06 +0400

    Feature added axios instatnce

--- next.config.js
+++ next.config.js
@@ -30,4 +30,8 @@ module.exports = {
             },
         ]
     },
+    publicRuntimeConfig: {
+        // Will be available on both server and client
+        backendUrl: process.env.BACKEND_URL,
+    },
 }
